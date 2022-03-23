6d0c3168502799928fedc5545d0e2b8baedaa26d
James Katarikawe <jpkatarikawe@gmail.com>
2022-03-22 20:43:24 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/meet/precache-manifest.b2b068cb198075329b50fc8590899b59.js"
+  "/meet/precache-manifest.afb79d66fd283a00dcbc8a2557a28e52.js"
 );
 
 workbox.clientsClaim();
