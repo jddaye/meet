b6405d1454607f943c66ec4142974a261a090922
Jessica Daye <jddaye@hotmail.com>
2022-03-30 08:15:12 -0400

    eslint suggestions

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -2,17 +2,21 @@ import React, { useEffect, useState } from 'react';
 import { PieChart, Pie, Cell, ResponsiveContainer } from 'recharts';
 
 const EventGenre = ({ events }) => {
-    const [data, setData] = useState([]);
-    const getData = () => {
-      const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS'];
-      const data = genres.map((genre) => {
-        const value = events.filter((event) => event.summary.split(' ').includes(genre)).length;
-        return { name: genre, value };
-      });
-      return data;
-    };
+const [data, setData] = useState([]);
 
-    useEffect(() => { setData(() => getData()); }, [events]);
+
+    useEffect(() => { 
+        const getData = () => {
+            const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS'];
+            const data = genres.map((genre) => {
+              const value = events.filter((event) => 
+                event.summary.split(' ').includes(genre)).length;
+              return { name: genre, value };
+            });
+            return data;
+          };
+        setData(() => getData()); 
+    }, [events]);
 
     const colors = [
         '#8ECAE6',
--- src/api.js
+++ src/api.js
@@ -74,7 +74,7 @@ export const getEvents = async () => {
 
     if (token) {
         removeQuery();
-        const url = 'https://3qclnxpwj3.execute-api.eu-central-1.amazonaws.com/dev/api/get-events' + '/' + token;
+        const url = 'https://3qclnxpwj3.execute-api.eu-central-1.amazonaws.com/dev/api/get-events/' + token;
         const result = await axios.get(url);
         if (result.data) {
             var locations = extractLocations(result.data.events);
